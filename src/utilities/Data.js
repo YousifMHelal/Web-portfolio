712d7f20de21654b82f9c2d6ec8044a50ef31b6d
YousifMHelal <yousifm.helal@gmail.com>
2023-05-15 14:52:41 +0300

    adding new project

--- src/utilities/Data.js
+++ src/utilities/Data.js
@@ -1,43 +1,53 @@
-import project1 from '../assets/project-1.png'
-import project2 from '../assets/project-2.png'
-import project3 from '../assets/Project-3.png'
-import project4 from '../assets/project-4.png'
+import project1 from "../assets/project-1.png";
+import project2 from "../assets/project-2.png";
+import project3 from "../assets/Project-3.png";
+import project4 from "../assets/project-4.png";
+import project5 from "../assets/project-5.png";
 
 export const Data = [
     {
-        id: '001',
-        name: 'Ui/Ux Designer Portfolio',
+        id: "001",
+        name: "Ui/Ux Designer Portfolio",
         img: project1,
-        info: 'I created this website to showcase your skills and experience as a Ui/Ux designer. I designed this website to highlight Your work and provide visitors with a comprehensive overview of your skills, projects, and achievements.',
-        techs: ['html', 'css', 'javascript'],
-        github: 'https://yousifmhelal.github.io/portfolio',
-        color: ['bg-html', 'bg-css', 'bg-javascript']
+        info: "I created this website to showcase your skills and experience as a Ui/Ux designer. I designed this website to highlight Your work and provide visitors with a comprehensive overview of your skills, projects, and achievements.",
+        techs: ["html", "css", "javascript"],
+        github: "https://yousifmhelal.github.io/portfolio",
+        color: ["bg-html", "bg-css", "bg-javascript"],
     },
     {
-        id: '002',
-        name: 'Tic Tac Toe Game',
+        id: "005",
+        name: "E-commerce Application",
+        img: project5,
+        info: "Using my expertise in web development and e-commerce platforms, I built the website using modern technologies such as React, JavaScript, and Css. The website was optimized for speed and performance, ensuring that customers could browse and purchase products quickly and efficiently.",
+        techs: ["react", "javascript", "redux", "tailwind"],
+        github: "https://yousifmhelal.github.io/E-commerce",
+        color: ["bg-react", "bg-javascript", "bg-redux", "bg-tailwind"],
+    },
+    {
+        id: "002",
+        name: "Tic Tac Toe Game",
         img: project2,
-        info: 'Using my expertise in JavaScript programming, I built this game to be fast, reliable, and easy to use. The game includes a range of features, including a simple and intuitive user interface, responsive design.',
-        techs: ['html', 'css', 'javascript'],
-        github: 'https://yousifmhelal.github.io/tic-tac-toe',
-        color: ['bg-html', 'bg-css', 'bg-javascript']
+        info: "Using my expertise in JavaScript programming, I built this game to be fast, reliable, and easy to use. The game includes a range of features, including a simple and intuitive user interface, responsive design.",
+        techs: ["html", "css", "javascript"],
+        github: "https://yousifmhelal.github.io/tic-tac-toe",
+        color: ["bg-html", "bg-css", "bg-javascript"],
     },
     {
-        id: '003',
-        name: 'Movies List Website',
+        id: "003",
+        name: "Movies List Website",
         img: project3,
-        info: 'In this project I integrated the website with a third-party API, allowing users to access up-to-date information about their favorite movies.',
-        techs: ['react', 'redux', 'bootstrap'],
-        github: 'https://yousifmhelal.github.io/movies-list',
-        color: ['bg-react', 'bg-redux', 'bg-bootstrap']
+        info: "In this project I integrated the website with a third-party API, allowing users to access up-to-date information about their favorite movies.",
+        techs: ["react", "javascript", "redux", "bootstrap"],
+        github: "https://yousifmhelal.github.io/movies-list",
+        color: ["bg-react", "bg-javascript", "bg-redux", "bg-bootstrap"],
     },
     {
-        id: '004',
-        name: 'Todo App',
+        id: "004",
+        name: "Todo App",
         img: project4,
-        info: 'I worked on this project designing and developing a Todo App using React, a powerful JavaScript library. The Todo App was designed to help users manage their daily tasks and improve. their productivity.',
-        techs: ['react', 'tailwind'],
-        github: 'https://yousifmhelal.github.io/To-Do-App',
-        color: ['bg-react', 'bg-tailwind']
-    }
-]
+        info: "I worked on this project designing and developing a Todo App using React, a powerful JavaScript library. The Todo App was designed to help users manage their daily tasks and improve. their productivity.",
+        techs: ["react", "javascript", "tailwind"],
+        github: "https://yousifmhelal.github.io/To-Do-App",
+        color: ["bg-react", "bg-javascript", "bg-tailwind"],
+    },
+];
